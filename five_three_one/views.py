dad4c9e5710d93d53ea622ba668eafa5df5c6f9b
Ben <benf97@gmail.com>
2021-06-21 19:12:56 +0100

    531

--- five_three_one/views.py
+++ five_three_one/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse("<h1>531<h1>")
+    context = {
+        "week1": {
+            "set1": "ORM",
+            "set2": "ORM",
+            "set3": "ORM",
+        },
+        "week2": {
+            "set1": "ORM",
+            "set2": "ORM",
+            "set3": "ORM",
+        },
+        "week3": {
+            "set1": "ORM",
+            "set2": "ORM",
+            "set3": "ORM",
+        },
+        "week4": {
+            "set1": "ORM",
+            "set2": "ORM",
+            "set3": "ORM",
+        }
+    }
+    if (request.GET.get('mybtn')):
+        onerm = float(request.GET.get('submit_form'))
+        context["week1"]["set1"] = "{:.2f}".format(onerm * 0.65)
+        context["week1"]["set2"] = "{:.2f}".format(onerm * 0.75)
+        context["week1"]["set3"] = "{:.2f}".format(onerm * 0.85)
+        context["week2"]["set1"] = "{:.2f}".format(onerm * 0.70)
+        context["week2"]["set2"] = "{:.2f}".format(onerm * 0.80)
+        context["week2"]["set3"] = "{:.2f}".format(onerm * 0.90)
+        context["week3"]["set1"] = "{:.2f}".format(onerm * 0.75)
+        context["week3"]["set2"] = "{:.2f}".format(onerm * 0.85)
+        context["week3"]["set3"] = "{:.2f}".format(onerm * 0.95)
+        context["week4"]["set1"] = "{:.2f}".format(onerm * 0.4)
+        context["week4"]["set2"] = "{:.2f}".format(onerm * 0.5)
+        context["week4"]["set3"] = "{:.2f}".format(onerm * 0.6)
+
+    return render(request, '531/main.html', context)
